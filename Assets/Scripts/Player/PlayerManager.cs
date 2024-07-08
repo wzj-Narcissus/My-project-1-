57b38dce2f15ed03ae9ef2dfc3191f45129128e4
zhangshengxiang682 <2570041484@qq.com>
2024-07-08 23:39:52 +0800

    实现基本移动功能

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private Rigidbody2D theRB;
+    public float speed;
+
+    public float minDistance = 0.1f; 
+    public float maxDistance = 10.0f; 
+
+    private Vector2 targetPosition; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float distance = Vector2.Distance(transform.position, targetPosition);
+
+        float moveSpeed = speed * (distance / maxDistance);
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
     }
 }
