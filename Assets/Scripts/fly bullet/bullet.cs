3e4155615777c03e261b15a8c305656fdf182c56
wzj-Narcissus <2562404991@qq.com>
2024-07-10 21:21:32 +0800

    场景3

--- Assets/Scripts/fly bullet/bullet.cs
+++ Assets/Scripts/fly bullet/bullet.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    PlayerManager playerManager;
+
     public float speed = 10f;  // 子弹的移动速度
     public float livetime = 5;
     private Vector2 direction; // 子弹的飞行方向
     private float haslivetime=0;
-
+    private void Start()
+    {
+        playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+    }
     void Update()
     {
         haslivetime += Time.deltaTime;
@@ -27,6 +32,19 @@ public class bullet : MonoBehaviour
     public void SetDirection(Vector2 newDirection)
     {
         direction = newDirection.normalized; // 设置新的飞行方向，并确保是单位向量
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            playerManager.GetDamaged(1f + playerManager.inthurt);
+            Destroy(gameObject);
+        }
+        //if (other.gameObject.CompareTag("Env"))
+        //{
+        //    Destroy(gameObject);
+        //}
     }
 
 }
