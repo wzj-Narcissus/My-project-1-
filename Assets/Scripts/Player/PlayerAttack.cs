18593e348f8fddd16acf0085169177a922e3659f
Han Gao <131942469+Zheni5D@users.noreply.github.com>
2024-07-11 19:15:12 +0800

    野猪动画

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private Animator _Animator;
     public GameObject player;
     PlayerManager playerManager;
     public float attackSpeed;
@@ -12,8 +13,16 @@ public class PlayerAttack : MonoBehaviour
     void Start()
     {
         playerManager = player.GetComponent<PlayerManager>();
+        _Animator = GetComponent<Animator>();
     }
-
+    public void SetAFalse()//将攻击动画关闭函数
+    {
+        _Animator.SetBool("isAttacking", false);
+    }
+    public void SetHFalse()//将受击动画关闭函数
+    {
+        _Animator.SetBool("isHited", false);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,6 +32,9 @@ public class PlayerAttack : MonoBehaviour
     {
         //transform.position = Vector2.MoveTowards(transform.position, target, attackSpeed * Time.deltaTime);
         playerManager.theRB.velocity = target.normalized*attackSpeed;
-        
+        _Animator.SetBool("isAttacking", true);
+        Invoke("SetAFalse", 0.1f);
+
+
     }
 }
